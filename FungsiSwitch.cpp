231ef0c9ae3c4382ace27edc5b73328e9f044c5f
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-03-26 14:36:30 +0700

    membuat prosedur input data

--- FungsiSwitch.cpp
+++ FungsiSwitch.cpp
@@ -5,3 +5,11 @@
 using namespace std;
 
 int bilangan1, bilangan2;
+
+void inputData()
+{
+	cout << "Masukkan bilangan pertama : ";
+	cin >> bilangan1;
+	cout << "Masukkan bilangan kedua : ";
+	cin >> bilangan2;
+}
