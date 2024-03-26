ec832825a94a0f2963ebe3f0cf9340b3f1e364f1
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-03-26 14:51:33 +0700

    membuat menu pilihan

--- FungsiSwitch.cpp
+++ FungsiSwitch.cpp
@@ -37,4 +37,18 @@ float pembagian(int a, int b)
 int main()
 {
 	int pilihan;
+	
+	do
+	{
+		cout << "Kalkulator Sederhana" << endl;
+		cout << "=====================" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cout << "=====================" << endl;
+		cout << "Masukkan menu pilihan : ";
+		cin >> pilihan;
+	}
 }
